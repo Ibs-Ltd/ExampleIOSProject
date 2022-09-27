e8f65940cf066f068d83b33a5068f5a41d5d7702
Faizan <m.faizan0786@gmail.com>
2022-09-27 16:51:13 +0530

    Initial Commit

---
+++ Shared/ExampleIOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleIOSProjectApp.swift
+//  Shared
+//
+//  Created by Mohammad Faizan on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleIOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
